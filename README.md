b5ce49c1e1154d2c5d7c07edf4204143ee86e445
aily-david-jimenez <david.jimenez@ailylabs.com>
2024-03-03 18:30:55 +0100

    Initial commit from Create Next App

---
+++ README.md
@@ -0,0 +1,7 @@
+This is a [Next.js](https://nextjs.org/) project template for KeepCoding Advanced React Module.
+
+Use this template doing the following
+
+```bash
+npx create-next-app@latest kc-nextjs --use-npm --example "https://github.com/davidjj76/kc-nextjs-template"
+```
---
+++ app/page.tsx
@@ -0,0 +1,15 @@
+export default function Home() {
+  return (
+    <div>
+      <header>
+        <h1>Hello, Keepcoders!</h1>
+      </header>
+      <main>
+        <p>Welcome to the Keepcoding React Advanced Module</p>
+        <p>
+          Let&#39;s start with <strong>Next.js</strong>
+        </p>
+      </main>
+    </div>
+  );
+}
