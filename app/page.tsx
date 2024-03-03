a8930a2319025cd9b82b9ff2472567c11d02626a
aily-david-jimenez <david.jimenez@ailylabs.com>
2024-03-03 18:46:13 +0100

    Add styles via tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 export default function Home() {
   return (
-    <div>
-      <header>
-        <h1>Hello, Keepcoders!</h1>
+    <div className="flex flex-col h-screen">
+      <header className="flex items-center h-20 px-6 border-solid border-b-2 bg-sky-400">
+        <h1 className="text-white font-semibold text-xl">Hello, Keepcoders!</h1>
       </header>
-      <main>
-        <p>Welcome to the Keepcoding React Advanced Module</p>
+      <main className="flex-1 text-gray-800 p-6">
+        <p className="mb-2">Welcome to the Keepcoding React Advanced Module</p>
         <p>
           Let&#39;s start with <strong>Next.js</strong>
         </p>
