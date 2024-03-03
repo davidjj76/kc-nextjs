da52d45b24caad34b615028f76e8184e558110a3
aily-david-jimenez <david.jimenez@ailylabs.com>
2024-03-03 18:52:26 +0100

    Add fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,24 @@
+import clsx from 'clsx';
+import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
+
 export default function Home() {
   return (
     <div className="flex flex-col h-screen">
-      <header className="flex items-center h-20 px-6 border-solid border-b-2 bg-sky-400">
-        <h1 className="text-white font-semibold text-xl">Hello, Keepcoders!</h1>
+      <header className="flex items-center h-20 px-6 border-solid border-b-2 bg-slate-900">
+        <Image
+          src="/Keepcoding.webp"
+          width={202}
+          height={48}
+          alt="Keepcoding tech school"
+        />
       </header>
       <main className="flex-1 text-gray-800 p-6">
+        <h1
+          className={clsx(lusitana.className, 'text-2xl text-orange-600 mb-8')}
+        >
+          Hello, Keepcoders!
+        </h1>
         <p className="mb-2">Welcome to the Keepcoding React Advanced Module</p>
         <p>
           Let&#39;s start with <strong>Next.js</strong>
