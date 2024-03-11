250b03eb4e8612be7f9e043a1ed3d743403e7e0b
aily-david-jimenez <david.jimenez@ailylabs.com>
2024-03-11 23:00:47 +0100

    fix error styles

--- app/courses/[slug]/error.tsx
+++ app/courses/[slug]/error.tsx
@@ -10,7 +10,7 @@ export default function Error({
   reset: () => void;
 }) {
   return (
-    <div className="flex justify-between items-center bg-error bg-opacity-10 rounded-md border-4 border-dashed border-error p-8">
+    <div className="sm:flex justify-between items-center bg-error bg-opacity-10 rounded-md border-4 border-dashed border-error p-8">
       <div>
         <h2 className="text-2xl font-bold tracking-tight text-error sm:text-4xl">
           Oooooops!
@@ -19,7 +19,7 @@ export default function Error({
       </div>
       <Link
         href="/courses"
-        className="rounded-md bg-error px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary"
+        className="text-center block mt-4 rounded-md bg-error px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary"
       >
         Go to courses catalog <span aria-hidden="true">→</span>
       </Link>
--- app/courses/new/error.tsx
+++ app/courses/new/error.tsx
@@ -8,7 +8,7 @@ export default function Error({
   reset: () => void;
 }) {
   return (
-    <div className="flex justify-between items-center bg-error bg-opacity-10 rounded-md border-4 border-dashed border-error p-8">
+    <div className="sm:flex justify-between items-center bg-error bg-opacity-10 rounded-md border-4 border-dashed border-error p-8">
       <div>
         <h2 className="text-2xl font-bold tracking-tight text-error sm:text-4xl">
           Oooooops!
@@ -16,7 +16,7 @@ export default function Error({
         <p className="mt-2 text-lg leading-8 text-error">{error.message}</p>
       </div>
       <button
-        className="rounded-md bg-error px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary"
+        className="w-full sm:w-auto mt-4 sm:mt-0 rounded-md bg-error px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-red-600 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary"
         onClick={() => reset()}
       >
         Retry
